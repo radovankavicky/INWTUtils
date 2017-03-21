ef257875cdddd85ec20e968c8b3a0cd7fb67d876
miraKlein <mcklein12@gmail.com>
2017-03-21 17:57:40 +0100

    Check style of own package (excluding specific files in inst)

--- tests/testthat/test-00_packageCodeStyle.R
+++ tests/testthat/test-00_packageCodeStyle.R
@@ -1,5 +1,27 @@
 context("Package code style")
 
 test_that("Code is lint-free", {
+
+  # Files to exclude from linter check
+  excludedFiles <- c("testScript_args_without_default_first_linter.R",
+                     "exampleScript.R",
+                     "testScript_checkStyle.R")
+
+  exclusions <- as.list(paste0(if (interactive()) "inst/" else "",
+                               excludedFiles))
+
+  writeLines(c('linters: with_defaults()',
+               paste0('exclusions: list("',
+                      paste0(exclusions, collapse = '", "'),
+                      '")'),
+               'exclude: "# nolint"',
+               'exclude_start: "# nolint start"',
+               'exclude_end: "# nolint end"'),
+             ".lintr")
+
+  lintr:::read_settings(".lintr")
+
   expect_lint_free(linters = selectLntrs())
+
+  unlink(".lintr")
 })
