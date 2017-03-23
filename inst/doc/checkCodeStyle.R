d473e67d3491f5e699135e7f21f90581af43f04d
miraKlein <mcklein12@gmail.com>
2017-03-23 10:24:39 +0100

    Improved checkStyle vignette

---
+++ inst/doc/checkCodeStyle.R
@@ -0,0 +1,36 @@
+## ---- echo = FALSE-------------------------------------------------------
+library(INWTUtils)
+
+## ----badStyleScript, eval = FALSE----------------------------------------
+#  writeLines(c("# This is an example for bad style",
+#               "x = 1+2",
+#               "# A comment with  double  spaces",
+#               "foo <- function( y) print(paste('You entered', y))",
+#               ""),
+#             con = "badStyle1.R")
+#  writeLines(c("# This is a second example   ",
+#               "z<-c(1,2)"),
+#             con = "badStyle2.R")
+
+## ----checkStyle, results = "hide", eval = FALSE--------------------------
+#  checkStyle(files = c("badStyle1.R", "badStyle2.R"),
+#             type = "script")
+
+## ----lintersAlways, results = 'asis', echo = FALSE-----------------------
+cat("-", paste0(names(selectLntrs()), collapse = "\n\n- "))
+
+## ----lintersScript, results = 'asis', echo = FALSE-----------------------
+cat("-", paste0(names(INWTUtils:::scriptLntrs()), collapse = "\n\n- "))
+
+## ----lintersFuns, results = 'asis', echo = FALSE-------------------------
+cat("-", paste0(names(INWTUtils:::pkgFunLntrs()), collapse = "\n\n- "))
+
+## ----nolint, eval = FALSE------------------------------------------------
+#  # nolint start
+#  x <- c(1,2) # This line will be excluded from the checks
+#  # nolint end
+#  y <- c(3, 4) # This line won't be excluded anymore.
+
+## ----rmFiles, echo = FALSE, results = 'hide', eval = FALSE---------------
+#  lapply(c("badStyle1.R", "badStyle2.R"), unlink)
+
