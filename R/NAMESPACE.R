3ef5ddc44e13945b71402fa8e882bb8406cde3e5
miraKlein <mcklein12@gmail.com>
2017-03-23 18:05:19 +0100

    removed multiple_dots_linter

--- R/NAMESPACE.R
+++ R/NAMESPACE.R
@@ -2,7 +2,7 @@
 #' @importFrom dplyr %>% contains
 #' @importFrom lintr expect_lint_free lint assignment_linter closed_curly_linter
 #' commas_linter infix_spaces_linter Lint line_length_linter no_tab_linter
-#' object_usage_linter multiple_dots_linter object_length_linter
+#' object_usage_linter object_length_linter
 #' open_curly_linter spaces_inside_linter spaces_left_parentheses_linter
 #' trailing_blank_lines_linter trailing_whitespace_linter
 #' @importFrom utils sessionInfo
