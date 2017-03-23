452cc24114db0ead76601ffd1ea6bb11e9c1ec86
miraKlein <mcklein12@gmail.com>
2017-03-23 13:39:08 +0100

    Vignette createProjectSkeleton

---
+++ inst/doc/createProjectSkeleton.R
@@ -0,0 +1,11 @@
+## ---- echo = FALSE, eval = FALSE-----------------------------------------
+#  library(INWTUtils)
+#  createProjectSkeleton("../myfolder")
+
+## ---- echo, eval = FALSE-------------------------------------------------
+#  createProjectSkeleton(dir = "playWith",
+#                        pkgName = "playPkg",
+#                        pkgOnToplevel = TRUE, # Default; could be left out
+#                        rProject = FALSE,
+#                        exampleScript = FALSE)
+
