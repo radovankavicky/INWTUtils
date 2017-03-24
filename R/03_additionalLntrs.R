71fa339358e685e687d9ea0dafb5bc220e617630
miraKlein <mcklein12@gmail.com>
2017-03-24 11:36:10 +0100

    Commented regular expressions for linters in code

--- R/03_additionalLntrs.R
+++ R/03_additionalLntrs.R
@@ -39,6 +39,19 @@ args_without_default_first_linter <- function(source_file) {
   pattern <- paste0("function\\([^\\)]*[A-z0-9_\\. '\"]+=[A-z0-9_\\. '\"]+,",
                     "[ ]*",
                     "[A-z0-9_\\. '\"]+[^(\\.\\.\\.)][\\),]+")
+  ## Argument with default value
+  # String "function("
+  # Any number of characters which are not ")"
+  # At least one character of A-z, 0-9, "_", ".", space, and quotes (name of an
+    # argument)
+  # Exaclty one "="
+  # Default value of the argument
+  # Comma separating this argument from the next one
+  ## Arbitrary number of spaces
+  ## Argument without default value
+  # Name of an argument
+  # NOT "..." (since this would be allowed)
+  # Closing bracket or comma (without having defined a default value)
 
   # Problems within lines
   idsWithin <- grep(pattern, source_file$file_lines)
@@ -68,12 +81,14 @@ args_without_default_first_linter <- function(source_file) {
 #' @describeIn INWTlinters Are there double whitespaces?
 #' @export
 double_space_linter <- function(source_file) {
+
   ids <- grep("[^#' ]+.*[^ ]+ {2,}[^( +#$)]", source_file$file_lines)
   # At least one character which is neither # nor '
   # Any characters
   # At least one character which is not space
   # At least two spaces
   # ...not directly followed by some spaces and a hash
+
   lapply(ids, function(id) {
     Lint(filename = source_file$filename,
          line_number = id,
@@ -90,7 +105,12 @@ double_space_linter <- function(source_file) {
 #' (only for scripts)
 #' @export
 internal_INWT_function_linter <- function(source_file) {
+
   ids <- grep("INWT\\w*:::", source_file$file_lines)
+  # String INWT
+  # Arbitrary number of word characters completing the package name
+  # "..." to call internal function
+
   lapply(ids, function(id) {
     Lint(filename = source_file$filename,
          line_number = id,
@@ -107,7 +127,12 @@ internal_INWT_function_linter <- function(source_file) {
 #' can have unexpected side effects. (only for package functions)
 #' @export
 setwd_linter <- function(source_file) {
+
   ids <- grep("^[^#\'\"]*setwd\\(", source_file$file_lines)
+  # Starting with arbitrary number of characters which are NOT # or quotes (to
+    # exclude comments and quoted text)
+  # String "setwd("
+
   lapply(ids, function(id) {
     Lint(filename = source_file$filename,
          line_number = id,
@@ -123,7 +148,12 @@ setwd_linter <- function(source_file) {
 #' unexpected side effects. (only for package functions)
 #' @export
 source_linter <- function(source_file) {
+
   ids <- grep("^[^#\'\"]*source\\(", source_file$file_lines)
+  # Starting with arbitrary number of characters which are NOT # or quotes (to
+    # exclude comments and quoted text)
+  # String "source("
+
   lapply(ids, function(id) {
     Lint(filename = source_file$filename,
          line_number = id,
