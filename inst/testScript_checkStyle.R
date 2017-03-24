55b6fd53d9a172911fa3ba4f3736a20f9d0c2465
miraKlein <mcklein12@gmail.com>
2017-03-24 14:16:15 +0100

    Expanded test for checkStyle()

--- inst/testScript_checkStyle.R
+++ inst/testScript_checkStyle.R
@@ -1,6 +1,13 @@
 # This script contains an example to test the checkStyle function.
+f1 <- function(x = 3, y) NULL # args_without_default_first_linter
+a = 2 # assignment_linter
+b <- c(a,2) # commas_linter
+c <-  1:5 # double_space_linter
+d # A comment with double  whitespace to check the double_space_linter
+f2 <- function(vec) 2*vec # infix_spaces_linter
+z <- 3 * a + b # A very long line with more than one hundred characters to check the line_length_linter
+veryLongVariableNameExceeding30Characters <- 0 # object_length_linter
+INWTUtils:::scriptLntrs() # internal_INWT_function_linter
+if(x == 1) 2
+# trailing_blank_lines_linter:
 
-x <-  1:5
-makeDouble <- function(vec) 2*vec
-
-y <- makeDouble(x)
--- tests/testthat/test-02a_checkStyle.R
+++ tests/testthat/test-02a_checkStyle.R
@@ -10,8 +10,28 @@ test_that("checkStyle returns correct type", {
 
 
 test_that("checkStyle returns correct content", {
-  erg <- checkStyle(system.file("testScript_checkStyle.R",
-                                package = "INWTUtils"), type = "script")
-  expect_true(erg[[1]]$message == "Double whitespace.")
-  expect_true(erg[[2]]$message == "Put spaces around all infix operators.")
+  erg <- checkStyle(
+    # system.file("testScript_checkStyle.R",
+    #                             package = "INWTUtils"),
+    "inst/testScript_checkStyle.R",
+    type = "script")
+  expect_true(erg[[1]]$message == paste("Arguments without default value should",
+                                        "be listed before\n         arguments",
+                                        "with default value."))
+  expect_true(erg[[2]]$message == "Use <-, not =, for assignment.")
+  expect_true(erg[[3]]$message == "Commas should always have a space after.")
+  expect_true(erg[[4]]$message == "Double whitespace.")
+  expect_true(erg[[5]]$message == "Double whitespace.")
+  expect_true(erg[[6]]$message == "Put spaces around all infix operators.")
+  expect_true(erg[[7]]$message ==
+                "lines should not be more than 100 characters.")
+  expect_true(erg[[8]]$message == paste("Variable and function names should",
+                                        "not be longer than 30 characters."))
+  expect_true(erg[[9]]$message == paste("If internal functions are used, they",
+                                        "should rather be\n         documented",
+                                        "and exported."))
+  expect_true(erg[[10]]$message == paste("Place a space before left",
+                                         "parenthesis, except in a function",
+                                         "call."))
+  expect_true(erg[[length(erg)]]$message == "Trailing blank lines are superfluous.")
 })
